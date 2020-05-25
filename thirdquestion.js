92e878e7489dd0e61acf15bbd36234f8d109f123
sawankhanchi <61899261+sawankhanchi@users.noreply.github.com>
2020-05-25 15:09:48 +0530

    Create thirdquestion.js

---
+++ thirdquestion.js
@@ -0,0 +1,22 @@
+// for print odd value
+
+var n = 9;
+
+// write function code here
+
+for (let i = 0; i <= n; i++) 
+if (i%2 !== 0) {
+    console.log(i)
+}
+
+
+// for print even value
+
+var n = 9;
+
+// write function code here
+
+for (let i = 0; i <= n; i++) 
+if (i%2 == 0) {
+    console.log(i)
+}
